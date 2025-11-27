132632be95e9d6591000ed77963bd8f9ce5d3c8d
Ademola Joel <joelmtn7@gmail.com>
2025-11-27 09:32:27 +0100

    repare

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,28 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb+srv://joelmtn7:nb9O5wWHRImlEBpk@comtribution1.hkf12uw.mongodb.net/?appName=Comtribution1");
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://joelmtn7:nb9O5wWHRImlEBpk@comtribution1.hkf12uw.mongodb.net/?appName=Comtribution1");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
